6d8b1d482958dea41840ef6cd5133a4c9834eb27
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-07 18:16:08 +0530

    Time: 3 ms (38.46%) | Memory: 19.1 MB (89.67%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,14 +1,11 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        i=0
-        while(i<100):
-            sum=0
-            while(n):
-                sum+=(n%10)**2
-                n=n//10
-            if(sum==1):
-                return True
-            n=sum
-            i+=1
-        return False
+        seen=set()
+        while(n!=1):
+            sums=sum([(int(i))**2 for i in str(n)])
+            if(sums in seen):
+                return False
+            seen.add(sums)
+            n=sums
+        return True
             
