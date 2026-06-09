ffb55481e9a170f292e911ace47fa404658606d7
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-09 13:50:47 +0530

    Time: 51 ms (15.76%) | Memory: 20.2 MB (72.88%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -4,8 +4,7 @@ class Solution:
         for i in range(n+1):
             sum=0
             while(i>0):
-                last_bit=i&1
-                sum+=last_bit
+                sum+=i&1
                 i=i>>1
             ans.append(sum)
         return ans
