01f5b6e9011d10c3a28972bf42e9bc16dbc79e26
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-10 21:40:30 +0530

    https://www.hackerrank.com/challenges/recursive-digit-sum/problem
    Implement superDigit function to calculate the super digit of a number based on its string representation and a multiplier.

---
+++ recursive_digit_sum.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    st=str(n)
+    if (len(st)==1):
+        return st
+    else:
+        sum=0
+        for i in (st):
+            sum+=int(i)
+    sum=sum*k
+    return superDigit(sum,1)
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
