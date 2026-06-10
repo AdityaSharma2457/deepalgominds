2b040c5d570144d7575ecf587266236e1dc3d8bd
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-10 09:47:26 +0530

    Time: 6 ms (50.06%) | Memory: 20 MB (94.42%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,6 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans=[]
-        for i in range(n+1):
-            ans.append(i.bit_count())
-        return ans
+        dp=[0]*(n+1)
+        track_2=1
+
+        for i in range(1,n+1):
+            if (track_2*2==i):
+                dp[i]=1
+                track_2=i
+            else:
+                dp[i]=dp[i-track_2]+1
+        return dp
