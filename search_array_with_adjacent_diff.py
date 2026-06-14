3b55ebacebddc4b867d017366055d322cb987e1c
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-14 21:58:11 +0530

    https://www.geeksforgeeks.org/problems/searching-in-an-array-where-adjacent-differ-by-at-most-k0456/1
    Implement findStepKeyIndex method to locate index of x in arr.

---
+++ search_array_with_adjacent_diff.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    def findStepKeyIndex(self, arr, k, x):
+        
+        
+            
+        for i in range(len(arr)):
+            if(arr[i]==x):
+                return i
+        return -1
