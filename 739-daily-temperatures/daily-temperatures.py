d9d5f07de5392ccbe7957ecb5b85adb56bb29e97
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-20 16:22:52 +0530

    Time: 88 ms (75.63%) | Memory: 28.1 MB (79.15%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack=[0]
+        stack=[]
         n=len(temperatures)
         ans=[0]*n
         for i in range(n):
