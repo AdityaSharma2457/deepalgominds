2854150502681ca942819fc334ca661fae3809ca
Aditya Sharma <sharmadi.kota@gmail.com>
2026-06-23 07:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.33%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        stack=[]
+        depth=0
+        for  i in s:
+            if(i=="("):
+               if depth>0:
+                    stack.append("(")
+               depth+=1
+            else:
+                depth-=1
+                if(depth>0):
+                    stack.append(")")
+        return ''.join(stack)
