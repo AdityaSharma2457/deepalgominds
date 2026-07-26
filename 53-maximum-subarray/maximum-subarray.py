d08a3340bf9996c40aa008853826ee356d80b04e
Aditya Sharma <sharmadi.kota@gmail.com>
2026-07-26 10:42:04 +0530

    Time: 46 ms (23.39%) | Memory: 31.6 MB (5.83%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,9 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         "kadane's algorithm"
+        ln=len(nums)
         curr_sum=nums[0]
         max_sum=nums[0]
-        for i in range(1,len(nums)):
+        for i in range(1,ln):
             curr_sum=max(nums[i],curr_sum+nums[i])
             max_sum=max(max_sum,curr_sum)
         return max_sum
