00c624063319187bec13a31a30e935442ca9c51d
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-04 17:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,20 +3,22 @@ class Solution:
         li=[]
 
         for i in range(len(s)):
+            length=len(li)
+
             if s[i] in {"(","[","{"}:
                 li.append(s[i])
             elif s[i]==")":
-                if len(li)!=0 and li[-1]=="(":
+                if length!=0 and li[-1]=="(":
                     li.pop()
                 else:
                     return False
             elif s[i]=="]":
-                if len(li)!=0 and li[-1]=="[":
+                if length!=0 and li[-1]=="[":
                     li.pop()
                 else:
                     return False
             elif s[i]=="}":
-                if len(li)!=0 and li[-1]=="{":
+                if length!=0 and li[-1]=="{":
                     li.pop()
                 else:
                     return False
