387c854a4641251598187d6c493f0d65239c20d6
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-04 18:18:35 +0530

    Time: 12 ms (67.79%) | Memory: 32.4 MB (31.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -6,12 +6,5 @@ class Solution:
         #         return True
         # return False
 
-        hash_table=set()
-
-        for i in range(len(nums)):
-            if nums[i] in hash_table:
-                return True
-            else:
-                hash_table.add(nums[i])
-        return False
+        return 0!=len(nums)-len(set(nums))
             
