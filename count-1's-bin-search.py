ce7f81c18596a17d68ea2109cdc9e38809edc3a9
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-16 22:47:27 +0530

    Implement binary search to count 1's in array

---
+++ count-1's-bin-search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countOnes(self, arr):
+        start=0
+        end=len(arr)-1
+        
+        while(start<=end):
+            mid=(start+end)//2
+            
+            if(arr[mid]==0):
+                end=mid-1
+            elif(arr[mid]==1):
+                start=mid+1
+        return start
