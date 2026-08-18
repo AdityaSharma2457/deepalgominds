13749ceb9257a92b639d211dca647781fae3c869
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-18 17:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (28.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,35 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        ans1=-1
+        ans2=-1
+        # first occurence
+
         start=0
         end=len(nums)-1
-        
         while(start<=end):
             mid=(start+end)//2
 
-            if(nums[mid]==target):
-                i=mid
-                while( i<len(nums) and nums[i]==target ):
-                    i+=1
-                ans1=i-1
-                j=mid
-                while(j>=0 and nums[j]==target ):
-                    j-=1
-                ans2=j+1
-                return [ans2,ans1]
-            elif(nums[mid]<target):
-                start=mid+1
-            else:
+            if (nums[mid]==target):
+                ans1=mid
                 end=mid-1
-        return [-1,-1]
-            
+            elif (nums[mid]<target):
+                start=mid+1
+            elif(nums[mid]>target):
+                end=mid-1
+
+        #second occurence
+
+        start=0
+        end=len(nums)-1
+        while(start<=end):
+            mid=(start+end)//2
+
+            if (nums[mid]==target):
+                ans2=mid
+                start=mid+1
+            elif (nums[mid]<target):
+                start=mid+1
+            elif(nums[mid]>target):
+                end=mid-1
+        return [ans1,ans2]
