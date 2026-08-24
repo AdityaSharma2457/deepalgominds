0204fbdb00505ee5c16033e4a9cfe5c2ae7d4d47
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-24 18:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.72%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,14 +1,31 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return 0
-        for i in range(len(nums)):
-            if i==0:
-                if nums[0]>nums[1]:
-                    return 0
-            elif i==len(nums)-1:
-                if nums[i]>nums[i-1]:
-                    return i
-            else:
-                if nums[i-1]<nums[i] and nums[i]>nums[i+1]:
-                    return i 
+        # brute force
+        # if len(nums)==1:
+        #     return 0
+        # for i in range(len(nums)):
+        #     if i==0:
+        #         if nums[0]>nums[1]:
+        #             return 0
+        #     elif i==len(nums)-1:
+        #         if nums[i]>nums[i-1]:
+        #             return i
+        #     else:
+        #         if nums[i-1]<nums[i] and nums[i]>nums[i+1]:
+        #             return i
+
+        # apply binary search
+
+        start=0
+        end=len(nums)-1
+         #uphill downhill concept to decide which side should i move
+
+        while(start<end):
+
+            mid=(start+end)//2
+
+            if (nums[mid]>nums[mid+1]): #going downhill (peak must be in left part)
+                end=mid # not end=mid-1 because mid can be the peak too.
+            else: # going uphill(peak must be in right part)
+                start=mid+1 #not start=mid because it is smaller than the element ahead of it /it cant be a peak
+        return end
