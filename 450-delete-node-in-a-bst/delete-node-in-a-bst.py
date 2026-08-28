88148b7bb5f9c5b708598ce3ef1f3d534bf35ce0
Aditya Sharma <sharmadi.kota@gmail.com>
2026-08-28 12:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (17.79%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -6,30 +6,31 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def findmin(node):
-            while(node.left):
-                node=node.left
-            return node
-        if root is None:
-            return 
-        if key<root.val:
-            root.left=self.deleteNode(root.left,key)
-        elif key>root.val:
-            root.right=self.deleteNode(root.right,key)
-
-        else: #agar mil gaya
-        # if left node
-            if root.left is None:
-                return root.right
-        # if right node
-            if root.right is None:
-                return root.left
-            else: #if 2 Nodes to 2 ways se kar sakte hai yaa to right subtree ke smallest se replace karo ya to left subtree ke largest se
-                min_node=findmin(root.right)
-                root.val=min_node.val
-                root.right=self.deleteNode(root.right,min_node.val)
-        return root
-
-
-            
-
+        def add_a_node(root,target):
+            if root is None:
+                return target
+            if target.val<root.val:
+                root.left=add_a_node(root.left,target)
+            if target.val>root.val:
+                root.right=add_a_node(root.right,target)
+            return root
+        def delete(root):
+            if root is None:
+                return None
+            if key<root.val: 
+                root.left=delete(root.left)
+            elif key>root.val:
+                root.right=delete(root.right)
+            elif key==root.val:
+                if root.left and root.right:
+                    store=root.right
+                    root=root.left
+                    root=add_a_node(root,store)
+                elif root.left:
+                    root=root.left
+                elif root.right:
+                    root=root.right
+                else:
+                    root=None
+            return root
+        return delete(root)
